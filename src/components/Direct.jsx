6c2f61a818a5e3ac88eb929f0f8880a52cc49edc
Earthpasserby <iwuno68@gmail.com>
2024-11-26 11:37:05 +0100

    modifications

--- src/components/Direct.jsx
+++ src/components/Direct.jsx
@@ -5,14 +5,10 @@ import review from "../assets/Pngs/review.png";
 const Direct = () => {
   return (
     <>
-      <div className="bg-[#FEFCE6] h-[320px]">
-        <div
-          className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 "
-        >
-          <div className=" grid grid-cols-1 md:grid-cols-2 gap-2 px-24">
-            <div className="md:pt-[100px] pt-4 tablet:pt-[20px]  " 
-            
-            >
+      <div className="bg-[#FEFCE6] h-400px]">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-0 max-w-5xl mx-auto ">
+            <div className="md:pt-[100px] pt-4 tablet:pt-[20px]">
               <h2 className="text-[#08121D] md:text-[26px] tablet:text-[24px] font-bold leading-8 md:w-[400px] sm:w-[350px] text-[26px]">
                 Let Talent Apply Co-Pilot get you to where you need to be in few
                 seconds <span className="text-[#0967D2]  text-3xl">.</span>
@@ -22,7 +18,7 @@ const Direct = () => {
                 you with any information you need to find.
               </p>
             </div>
-            <div className="flex gap-2">
+            <div className="flex gap-2 ">
               <div className="pt-4">
                 <img src={review} className="w-full" alt="Review-img" />
               </div>
