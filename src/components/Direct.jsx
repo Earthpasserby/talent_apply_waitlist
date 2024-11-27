02611c5dd11691bd01dd9656cab7d37afed2aae5
Earthpasserby <iwuno68@gmail.com>
2024-11-27 09:02:29 +0100

    modifications

--- src/components/Direct.jsx
+++ src/components/Direct.jsx
@@ -8,7 +8,7 @@ const Direct = () => {
       <div className="bg-[#FEFCE6] h-400px]">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-0 max-w-5xl mx-auto ">
-            <div className="md:pt-[100px] pt-4 tablet:pt-[20px] pl-20">
+            <div className="lg:pt-[100px] pt-4 md:pt-12 lg:pl-[70px]  sm:pl-0">
               <h2 className="text-[#08121D] md:text-[26px] tablet:text-[24px] font-bold leading-8 md:w-[400px] sm:w-[350px] text-[26px]">
                 Let Talent Apply Co-Pilot get you to where you need to be in few
                 seconds <span className="text-[#0967D2]  text-3xl">.</span>
